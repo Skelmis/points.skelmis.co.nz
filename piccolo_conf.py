6b7a637096d877ed412b48872dbaffe26cbd2eb8
skelmis <ethanmkh2@gmail.com>
2024-03-01 21:48:44 +1300

    feat: add Docker things

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -1,16 +1,19 @@
-from piccolo.engine.postgres import PostgresEngine
+import os
+from dotenv import load_dotenv
 
+from piccolo.engine.postgres import PostgresEngine
 from piccolo.conf.apps import AppRegistry
 
-
+load_dotenv()
 DB = PostgresEngine(
     config={
-        "database": "piccolo_project",
-        "user": "postgres",
-        "password": "",
-        "host": "localhost",
-        "port": 5432,
-    }
+        "database": os.environ["POSTGRES_DB"],
+        "user": os.environ["POSTGRES_USER"],
+        "password": os.environ["POSTGRES_PASSWORD"],
+        "host": os.environ["POSTGRES_HOST"],
+        "port": int(os.environ["POSTGRES_PORT"]),
+    },
+    extensions=tuple(),
 )
 
 APP_REGISTRY = AppRegistry(
